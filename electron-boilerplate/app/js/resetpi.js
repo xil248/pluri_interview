016e30acb7bb7fb1e356d137717a98f4c5efd62c
Xinghang Li <xil248@ucsd.edu>
2017-03-13 15:47:20 -0700

    small update

--- electron-boilerplate/app/js/resetpi.js
+++ electron-boilerplate/app/js/resetpi.js
@@ -9,31 +9,28 @@ var ssh = new SSH({
 });
 
 
- // document.getElementById("reboot").onclick = rebootFunc;
- // document.getElementById("test").onclick = testFunc;
-
-	function rebootFunc() {
-		alert("Rebooting...");
-		//Restart the server
-		ssh.exec('sudo reboot', {
-    	err: function(stderr) {
-        	console.log(stderr); // this-does-not-exist: command not found 
-    	}
-		}).start();
-		ssh.exec('2/EQvWeWTGohwE7+ki8ju7VADi4IMz0neY3c04Md1u4=', {
-    	err: function(stderr) {
-        	console.log(stderr); // this-does-not-exist: command not found 
-    	}
-		}).start();
+function rebootFunc() {
+	alert("Rebooting...");
+	//Restart the server
+	ssh.exec('sudo reboot', {
+	err: function(stderr) {
+    	console.log(stderr); // this-does-not-exist: command not found 
 	}
-
-	function testFunc() {
-		alert("Print out last shutdown time ...");
-		//Print out last shutdown time in console
-		ssh.exec('last -x|grep shutdown | head -1', {
-    	out: function(stdout) {
-        	console.log(stdout);
-    	}
-		}).start();
+	}).start();
+	ssh.exec('2/EQvWeWTGohwE7+ki8ju7VADi4IMz0neY3c04Md1u4=', {
+	err: function(stderr) {
+    	console.log(stderr); // this-does-not-exist: command not found 
 	}
+	}).start();
+}
+
+function testFunc() {
+	alert("Print out last shutdown time ...");
+	//Print out last shutdown time in console
+	ssh.exec('last -x|grep shutdown | head -1', {
+	out: function(stdout) {
+    	console.log(stdout);
+	}
+	}).start();
+}
 
